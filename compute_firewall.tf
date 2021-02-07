0680f8201d9abbf723a2aa9a0a2970a578267a55
Nekoshita Yuki <>
2021-02-07 23:22:59 +0900

    feat: define resources for vm instance in subnet

---
+++ compute_firewall.tf
@@ -0,0 +1,22 @@
+resource "google_compute_firewall" "my_network" {
+  name    = "my-network-firewall"
+  network = google_compute_network.my_network.name
+
+  direction = "INGRESS"
+
+  # 通信を許可するprotocolとportを指定する
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  # 対象のVMインスタンスのタグを指定する
+  target_tags = ["bastion-tag"]
+  # VMインスタンスへのSSH接続元IPを指定する
+  source_ranges = ["0.0.0.0/0"]
+
+  # CloudLoggingにFlowLogログを出力したい場合は設定する
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+}
---
+++ compute_network.tf
@@ -0,0 +1,18 @@
+resource "google_compute_network" "my_network" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "my_subnetwork" {
+  name   = "my-subnetwork"
+  region = var.gcp_regions["tokyo"]
+
+  # サブネットで使用したい内部IPアドレスの範囲を指定する
+  ip_cidr_range = "10.0.0.0/16"
+  network       = google_compute_network.my_network.self_link
+
+  # CloudLoggingにFlowLogログを出力したい場合は設定する
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+}
